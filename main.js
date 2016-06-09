96666cdde45e6fec5fa3593827c8f7feeda47c3f
Ryan Douglas <ryan@luckydogdigital.com>
2016-06-08 20:00:27 -0600

    mondays work

--- main.js
+++ main.js
@@ -25,20 +25,25 @@ ADVANCED TRACK: 8-11
 
 // 1. Log your name to the console (as a string).
 console.log("Question 1");
-
+console.log("Your Name")
 
 
 
 // 2. Declare a new variable called 'name',
 //    and then log it to the console.
-
+var name = "Name"
+console.log(name)
 
 
 
 
 // 3. Declare two variables with number values.
 //    Add, subtract, multiply and divide them.
+var number1, number2;
 
+number1 = 8
+number2 = 3
+console.log(number1%number2)
 
 
 
@@ -47,27 +52,38 @@ console.log("Question 1");
 //    Create a third variable that concatenates the two strings
 //    that also places a space between the strings.
 
-
+var hey = "hi"
+var bye = "bye"
+var greeting = hey +" "+ bye
+console.log(greeting)
 
 
 
 // 5. Add, subract, multiply and divide the strings '5' and '3'.
 //    Observe the results and brieftly explain the results within the console.
 
-
+var one = "5"
+var two = "3"
+console.log(typeof one+two)
+console.log(typeof one-two)
+console.log(typeof one/two)
+console.log(typeof 5+two)
+console.log(typeof NaN);
+console.log((5 + 3 + Number(one)));
 
 
 
 // 6. Create an alert.
 
-
+/* alert('alert') */
 
 
 
 // 7. Create a prompt that asks a user to pick a number.
 //    Then double the answer.
 
-
+var promptData = prompt("Pick a number")
+console.log(promptData*2)
 
 
 // ADVANCED TRACK
@@ -76,8 +92,12 @@ console.log("Question 1");
 //    In a single command display/log the sentence:
 //    "My name is...I'm....years old, and I live in....."
 
+var name = prompt("What is your name?");
+var age = prompt("How old are you?");
+var city = prompt("Where do you live?")
 
 
+console.log("My name is ", name, " I'm ", age, " years old, and I live in ", city);
 
 
 // 9. Create an alert that uses the response from a prompt.
